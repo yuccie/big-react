56c496feb0d7b9311a2405b5ef363a2ec045fb48
dujichong <dujichong@didiglobal.com>
2023-12-17 00:12:08 +0800

    feat: 导出jsx

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,7 +1,7 @@
-import { jsx } from './src/jsx'
+import { jsx } from './src/jsx';
 
 // 这是当前模块的出口文件
 export default {
-    version: '0.0.0',
-    createElement: jsx
-}
+	version: '0.0.0',
+	createElement: jsx
+};
